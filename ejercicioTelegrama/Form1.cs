d0f91d06f3268f734d2e3131ce1098d8939d3ac7
IzanLlobregatRuiz <izallorui@alu.edu.gva.es>
2025-11-25 16:44:06 +0100

    Añadido código

--- ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/Form1.cs
@@ -21,5 +21,34 @@ namespace ejercicioTelegrama
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                int num1, num2, num3;
+                num1 = int.Parse(txtNum1.Text);
+                num2 = int.Parse(txtNum2.Text);
+                num3 = int.Parse(txtNum3.Text);
+                if (num1 > num2)
+                {
+                    num3 = num3 + 5;
+                }
+                if (num1 == num3)
+                {
+                    num2++;
+                }
+                if (num2 < num3)
+                {
+                    num1--;
+                }
+                if (num1 * num2 > num3)
+                {
+                    num3 = num2 + 1;
+                }
+                MessageBox.Show("Valores finales: \n num1: " + num1 + "\n num2: "
+               + num2 + "\n num3: " + num3);
+            }
+
+        }
     }
 }
